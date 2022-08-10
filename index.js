8e15c3e2717e4595523e024b74b3539d874c6e91
Marcos Mendoza <ashevalel2@gmail.com>
2022-08-10 15:15:46 -0300

    commit

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+const scoreEl = document.querySelector('#scoreEl')
+const startGameButton = document.querySelector('#startGameBtn')
+const modalEl = document.querySelector('#modalEl');
+const bigScoreEl = document.querySelector('#bigScoreEl');
+
 class Player{
     constructor(x,y,radius,color){
         this.x = x;
@@ -66,6 +71,7 @@ class Enemy{
     }
 }
 
+const friction = 0.98;
 class Particle{
     constructor(x,y,radius,color,velocity){
         this.x = x;
@@ -73,27 +79,45 @@ class Particle{
         this.radius = radius;
         this.color = color;
         this.velocity = velocity;
+        this.alpha = 1;
     }
 
     draw(){
+        c.save();
+        c.globalAlpha = this.alpha;
         c.beginPath();
         c.arc(this.x,this.y,this.radius,0,Math.PI * 2,false)
         c.fillStyle = this.color;
         c.fill();
+        c.restore();
     }
 
     update(){
         this.draw();
+        this.velocity.x *= friction;
+        this.velocity.y *= friction;
         this.x += this.velocity.x;
         this.y += this.velocity.y;
+        this.alpha -= 0.01;
     }
 }
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
-const player = new Player(x,y,15,'white');
-const projectiles = [];
-const enemies = [];
+let player = new Player(x,y,15,'white');
+let projectiles = [];
+let enemies = [];
+let particles = [];
+
+function init(){
+     player = new Player(x,y,15,'white');
+     projectiles = [];
+     enemies = [];
+     particles = [];
+     scoreEl.innerHTML = 0;
+     bigScoreEl.innerHTML = 0;
+     
+}
 
 
 function spawnEnemies(){
@@ -125,11 +149,21 @@ function spawnEnemies(){
 
 
 let animationId; 
+let score = 0;
+
 function animate(){
     animationId = requestAnimationFrame(animate);
     c.fillStyle = 'rgba(0,0,0,0.1)';
     c.fillRect(0,0,canvas.width,canvas.height);
     player.draw();  
+    particles.forEach((particle , index) => {
+        if(particle.alpha <= 0){
+            particles.splice(index,1);
+        }
+        else{
+            particle.update();
+        }
+    });
     projectiles.forEach((projectile, index)=>{
         projectile.update();
         if(projectile.x + projectile.radius < 0 || projectile.y - projectile.radius > canvas.width || 
@@ -145,15 +179,29 @@ function animate(){
 
       const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)   
       if(dist - enemy.radius - player.radius < 1){
+      
         cancelAnimationFrame(animationId);
+        modalEl.style.display ='flex';
+        bigScoreEl.innerHTML = score;
       }
       projectiles.forEach((projectile, projectileIndex) => {
         //Nos da la distancia del enemigo con las balas
         const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
         
         //Colision de bala con enemigo
-        if(dist - enemy.radius - projectile.radius < 1){   
+        if(dist - enemy.radius - projectile.radius < 1){  
+            
+          
+            //Creating particle explosion
+            for(let i = 0; i < enemy.radius * 2; i++){
+                particles.push(new Particle(projectile.x,projectile.y,Math.random() * 2,enemy.color,
+                {x:(Math.random() - 0.5) * Math.random() * 6, y:(Math.random() - 0.5) * Math.random() * 6}))
+            }
+            
+
             if(enemy.radius - 5 > 5){
+                score += 100;
+                scoreEl.innerHTML = score;
                 gsap.to(enemy,{
                     radius: enemy.radius - 5
                 })
@@ -162,6 +210,7 @@ function animate(){
                 }, 0);
             } 
             else{
+                score += 250;
                 setTimeout(() => {
                     enemies.splice(index,1);
                     projectiles.splice(projectileIndex,1);
@@ -182,5 +231,10 @@ addEventListener('click',(event) => {
     projectiles.push(new Projectile(x,y,5,'white',velocity));
 })
 
-animate();
-spawnEnemies();
+startGameButton.addEventListener('click',() => {
+    init();
+    animate();
+    spawnEnemies();
+    modalEl.style.display ='none';
+})
+
